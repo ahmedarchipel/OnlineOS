5e175e33c54582f27392d2dbf3febac16db34634
ahmedarchipel <ahmedzoghlemi.archipel@gmail.com>
2026-04-18 21:21:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@
 // ============================================================
 
 const SUPABASE_URL  = 'https://cmdjcyaqdmyvwiuyqsat.supabase.co';
-const SUPABASE_KEY  = 'sb_publishable_kNFEG0TUsejL8nUZJWkf1w_9jGpvq6d';
+const SUPABASE_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtZGpjeWFxZG15dndpdXlxc2F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MTYzNTcsImV4cCI6MjA5MjA5MjM1N30.WHI2krpD4MfLvDxQ_ix7vyo6JisKSvrQhPXRs5ZlyWc';
 
 // Nom de l'application (apparaît dans la barre latérale)
 const APP_NAME      = 'Archipel Online OS';
